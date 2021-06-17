736442e06126c36c37e64ed35270f38e8b13ef03
Proger2k <stolbetskiy.yura@gmail.com>
2021-06-17 16:40:25 +0300

    Solved the first part

---
+++ Course1/week4/Rational/Part1/cmake-build-debug/CMakeFiles/Part1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Part1.dir/main.cpp.obj"
+  "Part1.exe"
+  "Part1.exe.manifest"
+  "Part1.lib"
+  "Part1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Part1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
