a21657fc08af3bbbaaea2c2403531bef07cea9c7
Proger2k <stolbetskiy.yura@gmail.com>
2021-06-18 21:53:50 +0300

    Reading the date

--- Course1/final task/main.cpp
+++ Course1/final task/main.cpp
@@ -1,50 +1,131 @@
 #include <iostream>
 #include <string>
 #include <map>
-#include <set>
+#include <vector>
 
 using namespace std;
 
-// Реализуйте функции и методы классов и при необходимости добавьте свои
+
+string read_date(const string& date, int& index){
+    string result;
+
+    while(date[index] < '0' || date[index] >= '9'){
+        result += date[index];
+        index++;
+    }
+
+    result += date[index];
+    index++;
+
+    for(int i = index; i < date.size(); i++){
+        index = i;
+
+        if(date[index] == '-'){
+            break;
+        }
+
+        result += date[index];
+    }
+    index++;
+
+    return result;
+}
 
 class Date {
 public:
-    Date(int new_year, int new_month, int new_day){
-        year = new_year;
-        month = new_month;
-        day = new_day;
+    Date(const string& date){
+        if(date[date.size() - 1] <= '0' || date[date.size() - 1] >= '9'){
+            string message = "Wrong date format: " + date;
+            throw exception(message.c_str());
+        }
+
+        int index = 0;
+
+        string str_year = read_date(date, index);
+        string str_month = read_date(date, index);
+        string str_day = read_date(date, index);
+
+        try{
+            int check = stoi(str_year.c_str());
+
+            int size = str_year.size();
+            if(size > 4){
+                throw exception();
+            }
+
+            for(int i = size; i < 4; i++){
+                str_year += '0';
+            }
+        }catch (exception& ex){
+            string message = "Wrong date format: " + date;
+            throw exception(message.c_str());
+        }
+
+        try{
+            int check = stoi(str_day.c_str());
+
+            int size = str_year.size();
+            if(size > 2){
+                throw exception();
+            }
+
+            for(int i = size; i < 2; i++){
+                str_year += '0';
+            }
+        }catch (exception& ex){
+            string message = "Wrong date format: " + date;
+            throw exception(message.c_str());
+        }
+
     }
 
-    int GetYear() const{
+    string GetYear() const{
         return year;
     }
 
-    int GetMonth() const{
+    string GetMonth() const{
         return month;
     }
 
-    int GetDay() const{
+    string GetDay() const{
         return day;
     }
 
 private:
-    int year;
-    int month;
-    int day;
+    string year;
+    string month;
+    string day;
 };
 
 bool operator<(const Date& lhs, const Date& rhs);
 
 class Database {
 public:
-    void AddEvent(const Date& date, const string& event);
+    void AddEvent(const Date& date, const string& event){
+
+    }
+
     bool DeleteEvent(const Date& date, const string& event);
     int  DeleteDate(const Date& date);
 
     /* ??? */ //Find(const Date& date) const;
 
     void Print() const;
+
+private:
+    map<Date, vector<string>> date;
 };
+
+string get_line(string command, int& index){
+    string result = "";
+    while(command[index] != ' ' && command.size() > index){
+        result += command[index];
+        index++;
+    }
+    index++;
+
+    return result;
+}
 
 int main() {
     Database db;
@@ -52,6 +133,30 @@ int main() {
     string command;
     while (getline(cin, command)) {
         // Считайте команды с потока ввода и обработайте каждую
+        if(command.empty())
+            continue;
+
+        int index = 0;
+        string operation = get_line(command, index);
+
+        if(operation == "Add"){
+            string str_date = get_line(command, index);
+            string event = get_line(command, index);
+
+            try{
+                Date date(str_date);
+            }catch (exception& ex){
+                cout << "Wrong date format: " << ex.what();
+            }
+        }else if(operation == "Del"){
+
+        }else if(operation == "Find"){
+
+        }else if(operation == "Print"){
+
+        }else{
+            cout << "Unknown command: " << operation << endl;
+        }
     }
 
     return 0;
